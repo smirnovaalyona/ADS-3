9c36a8e9e7d56c974b495a322de4e86b31bf4d7c
smirnovaalyona <99326020+smirnovaalyona@users.noreply.github.com>
2022-03-11 22:10:47 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,7 +2,7 @@
 
 int cbinsearch(int *arr, int size, int value) {
   //  поместить сюда реализацию алгор
-  L = 0, R = size - 1, middle, numbers = 0;
+  int L = 0, R = size - 1, middle, numbers = 0;
   while (L < R) {
     middle = (L + R) / 2;
     if (arr[middle] < value)
