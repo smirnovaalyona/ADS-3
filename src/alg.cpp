d93bb69bd6bccadee7b959504b3e8f8efcb061ff
smirnovaalyona <99326020+smirnovaalyona@users.noreply.github.com>
2022-03-11 20:58:51 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,7 +1,7 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгори
+  //  поместить сюда реализацию алгор
   L = 0, R = size - 1, middle, numbers = 0;
   while (L < R) {
     middle = (L + R) / 2;
